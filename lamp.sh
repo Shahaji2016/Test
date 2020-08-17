623f5302ecdbec4000501540eb1e6f9eb3fcd072
Shahaji2016 <shahajinalawade5@gmail.com>
2020-08-17 18:36:18 +0530

    Added LAMP sh exxtension file

---
+++ lamp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=$1
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+#sudo aptitude purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
+
+# install the LAMP stack
+apt-get -y install apache2 mysql-server php php-mysql
+
+# write some PHP
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
+
+# restart Apache
+apachectl restart
+
